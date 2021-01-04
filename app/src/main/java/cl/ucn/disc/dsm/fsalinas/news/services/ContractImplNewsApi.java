f7c893fc390e87cd04aa7447ccdff7a1cdca338c
pipesalinass <37817401+pipesalinass@users.noreply.github.com>
2021-01-04 09:27:46 -0300

    fix(NewsApi): remove duplicates

--- app/src/main/java/cl/ucn/disc/dsm/fsalinas/news/services/ContractImplNewsApi.java
+++ app/src/main/java/cl/ucn/disc/dsm/fsalinas/news/services/ContractImplNewsApi.java
@@ -27,6 +27,11 @@ import org.threeten.bp.ZonedDateTime;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import cl.ucn.disc.dsm.fsalinas.news.model.News;
 import cl.ucn.disc.dsm.fsalinas.news.utils.Validation;
@@ -55,9 +60,21 @@ public class ContractImplNewsApi implements Contracts {
      */
     public ContractImplNewsApi(String apiKey) {
 
-        Validation.notNull(apiKey, "Apikey!");
+        Validation.minSize(apiKey,10, "Apikey!");
 
         this.newsApiService = new NewsApiService (apiKey);
+    }
+
+    /**
+     * Filter the Stream.
+     *
+     * @param idExtractor
+     * @param <T> news to filter.
+     * @return true if the news already exists.
+     */
+    private static <T> Predicate<T> distintById(Function<? super T, ?> idExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(idExtractor.apply(t), Boolean.TRUE) == null;
     }
 
     /**
@@ -80,11 +97,17 @@ public class ContractImplNewsApi implements Contracts {
             // Iterate over the articles.
             for(Article article: articles) {
                 // Article -> News
-                news.add(article2news(article));
+                news.add(toNews(article));
             }
 
             // .. return the list of News.
-            return news;
+            return news.stream()
+                    // Remove the duplicates by id
+                    .filter(distintById(News::getId))
+                    // Sort the stream by publishedAt
+                    .sorted((k1, k2) -> k2.getPublishedAt().compareTo(k1.getPublishedAt()))
+                    // Return the stream to list
+                    .collect(Collectors.toList());
 
         } catch (IOException e) {
             log.error("Error", e);
@@ -102,7 +125,7 @@ public class ContractImplNewsApi implements Contracts {
      * @param article to  convert.
      * @return the News.
      */
-    private static News article2news(Article article) {
+    private static News toNews(final Article article) {
 
         log.debug("Article: {}. ", ToStringBuilder.reflectionToString(
                 article, ToStringStyle.MULTI_LINE_STYLE
@@ -113,16 +136,28 @@ public class ContractImplNewsApi implements Contracts {
                 .parse(article.getPublishedAt())
                 .withZoneSameInstant(ZoneId.of("-3"));
 
+        Validation.notNull(article, "Article null");
+
+        // Warning message?
+        boolean needFix = false;
+
         // Fixing the restrictions.
-        if (article.getAuthor() == null) {
+        if (article.getAuthor() == null || article.getAuthor().length() == 0) {
             article.setAuthor("No Author*");
+            needFix = true;
         }
 
         // Fixing more restrictions
         if (article.getDescription() == null || article.getDescription().length() == 0) {
             article.setDescription("No Description*");
+            needFix = true;
         }
 
+        // .. yes, warning message.
+        if (needFix) {
+            // Debug of Article
+            log.warn("Article with invalid restrictions: {}." , ToStringBuilder.reflectionToString(article, ToStringStyle.MULTI_LINE_STYLE));
+        }
 
         return new News(
                 article.getTitle(),
