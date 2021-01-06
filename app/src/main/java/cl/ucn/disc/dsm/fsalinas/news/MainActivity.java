2a94ff1209f44725223b097c13ba311e3aca432d
pipesalinass <37817401+pipesalinass@users.noreply.github.com>
2021-01-06 14:35:07 -0300

    feat(Main): add recyclerView, fast adapter, night mode

--- app/src/main/java/cl/ucn/disc/dsm/fsalinas/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/fsalinas/news/MainActivity.java
@@ -19,68 +19,128 @@
 
 package cl.ucn.disc.dsm.fsalinas.news;
 
-        import android.os.Bundle;
-        import android.view.Menu;
-        import android.view.MenuItem;
+        import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ModelAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.fsalinas.news.model.News;
+import cl.ucn.disc.dsm.fsalinas.news.services.ContractImplNewsApi;
+import cl.ucn.disc.dsm.fsalinas.news.services.Contracts;
 
         /**
-         *  The Main Class.
-         * @author Felipe Salinas-Urra.
-         */
-
+* The Main Class.
+* @author Felipe Salinas-Urra.
+*/
 public class MainActivity extends AppCompatActivity {
 
-            /**
-             * Creates the night mode menu option.
-             *
-             * @param menu The menu in the action bar
-             * @return True to display the menu, false to hide it
-             */
-            @Override
-            public boolean onCreateOptionsMenu(Menu menu) {
-                getMenuInflater().inflate(R.menu.main_menu, menu);
-                // Change the label of the menu based on the state of the app.
-                int nightMode = AppCompatDelegate.getDefaultNightMode();
-                if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
-                    menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
-                } else{
-                    menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
-                }
-                return true;
-            }
+    /**
+     * The Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(MainActivity.class);
 
-            /**
-             * Handles options menu item clicks.
-             *
-             * @param item The item that was pressed
-             * @return returns true since the item click wa handled
-             */
-            @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-                // Check if the correct item was clicked.
-                if (item.getItemId() == R.id.night_mode) {
-                    // Get the night mode state of the app.
-                    int nightMode = AppCompatDelegate.getDefaultNightMode();
-                    // Set the theme mode for the restarted activity.
-                    if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
-                        AppCompatDelegate.setDefaultNightMode
-                                (AppCompatDelegate.MODE_NIGHT_NO);
-                    } else {
-                        AppCompatDelegate.setDefaultNightMode
-                                (AppCompatDelegate.MODE_NIGHT_YES);
+    /**
+     * The List View.
+     */
+    protected ListView listView;
+
+
+
+    /**
+     * Creates the night mode menu option.
+     *
+     * @param menu The menu in the action bar
+     * @return True to display the menu, false to hide it
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    /**
+     * Handles options menu item clicks.
+     *
+     * @param item The item that was pressed
+     * @return returns true since the item click wa handled
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check if the correct item was clicked.
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            // Set the theme mode for the restarted activity.
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
                     }
                     // Recreate the activity for the theme change to take effect.
                     recreate();
                 }
                 return true;
-            }
+    }
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        log.debug("OnCreate ..");
+        setContentView(R.layout.activity_main);
+
+        // The toolbar
+        this.setSupportActionBar(findViewById(R.id.am_t_toolbar));
+
+        // The FastAdapter
+        ModelAdapter<News, NewsItem> newsAdapter = new ModelAdapter<>(NewsItem::new);
+        FastAdapter<NewsItem> fastAdapter = FastAdapter.with(newsAdapter);
+        fastAdapter.withSelectable(false);
+
+        // The RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.am_rv_news);
+        recyclerView.setAdapter(fastAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        // Get the news in the background thread
+        AsyncTask.execute(() -> {
+
+            // Using the contracts to get the news ..
+            Contracts contracts = new ContractImplNewsApi("08681dc8269b4ac085551cfd3ad43b99");
+
+            // Get the News from NewsApi (Internet)
+            List<News> listNews = contracts.retrieveNews(30);
+
+            // Set the adapter
+            runOnUiThread(() -> {
+                newsAdapter.add(listNews);
+            });
+
+    });
+
+    }
 }
